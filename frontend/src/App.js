ab20ee2e6fe1179876ead47990868dc7093fb132
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2022-06-23 15:28:18 -0300

    reactView

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import data from "./data"
+function App() {
+    return (
+        <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">
+                        ServicioAhora!
+                    </a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sing In</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                    {
+                        data.products.map(products => (
+                            <div key={products._id} className="card">
+                                <a href={`/product/${products._id}`}>
+                                    <img src={products.image} alt={products.name} />
+                                </a>
+                                <div className="card-body">
+                                    <a href={`/product/${products._id}`}>
+                                        <h2>{products.name}</h2>
+                                    </a>
+                                    <div className="rating">
+                                        <span><i className="fa fa-star"></i></span>
+                                        <span><i className="fa fa-star"></i> </span>
+                                        <span><i className="fa fa-star"></i></span>
+                                        <span><i className="fa fa-star"></i></span>
+                                        <span><i className="fa fa-star"></i></span>
+                                        <div className="price">
+                                            ${products.price}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+
+                </div>
+            </main>
+            <footer className="row center">All right reserved</footer>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: "./images/NikeShirt.png",
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Currado paaa'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: "./images/AdidasShirt.png",
+            price: 122,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 11,
+            description: 'aaaaaaaaaaaaa '
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: "./images/LacosteShirt.png",
+            price: 150,
+            brand: 'Lacoste',
+            rating: 4,
+            numReviews: 11,
+            description: 'wwwwwwwwwww'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: "./images/NikePant.png",
+            price: 99,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 11,
+            description: 'wwwwwewe'
+        },
+        {
+            _id: '6',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: "./images/PumaPant.png",
+            price: 89,
+            brand: 'Puma',
+            rating: 5,
+            numReviews: 11,
+            description: 'wwwwwewe'
+        },
+
+        {
+            _id:'6',
+            name:'Adidas Slim Pant',
+            category:'Pants',
+            image:"./images/AdidasPant.png",
+            price: 99,
+            brand:'Adidas',
+            rating:4,
+            numReviews:11,
+            description:'wwwwwewe'
+},
+
+    ],
+};
+export default data;

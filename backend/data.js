bba2b6f53e7c39e7eeb3c467299677d55f3fad0c
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2022-06-24 13:11:24 -0300

    server

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: "../images/NikeShirt.png",
+            price: 100,
+            countInStock: 22,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Currado paaa'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: "../images/AdidasShirt.png",
+            price: 122,
+            countInStock: 0,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 11,
+            description: 'aaaaaaaaaaaaa '
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: "../images/LacosteShirt.png",
+            price: 150,
+            countInStock: 11,
+            brand: 'Lacoste',
+            rating: 4,
+            numReviews: 1,
+            description: 'wwwwwwwwwww'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: "../images/NikePant.png",
+            price: 99,
+            countInStock: 33,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 7,
+            description: 'wwwwwewe'
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: "../images/PumaPant.png",
+            price: 89,
+            countInStock: 100,
+            brand: 'Puma',
+            rating: 5,
+            numReviews: 3,
+            description: 'wwwwwewe'
+        },
+
+        {
+            _id:'6',
+            name:'Adidas Slim Pant',
+            category:'Pants',
+            image:"../images/AdidasPant.png",
+            price: 99,
+            countInStock: 2,
+            brand:'Adidas',
+            rating:3.5,
+            numReviews:5,
+            description:'wwwwwewe'
+},
+
+    ],
+};
+export default data;

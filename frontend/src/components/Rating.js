56400ca72e19c96561affac568ec309a87ba936b
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2022-06-27 17:12:59 -0300

    redux

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -6,19 +6,19 @@ export default function Rating(props) {
 
         <div className="rating">
             <span>
-                <i className={rating >= 1 ? 'fa fa-star' : rating >= 0.5 ? "fa fa-star-half" : 'fa fa-star-o'}></i>
+                <i className={rating >= 1 ? 'fa fa-star' : rating >= 0.5 ? "fa fa-star-half-alt" : 'fa fa-star-o'}></i>
             </span>
             <span>
-            <i className={rating >= 2 ? 'fa fa-star' : rating >= 1.5 ? "fa fa-star-half" : 'fa fa-star-o'}></i>
+            <i className={rating >= 2 ? 'fa fa-star' : rating >= 1.5 ? "fa fa-star-half-alt" : 'fa fa-star-o'}></i>
             </span>
             <span>
-            <i className={rating >= 3 ? 'fa fa-star' : rating >= 2.5 ? "fa fa-star-half" : 'fa fa-star-o'}></i>
+            <i className={rating >= 3 ? 'fa fa-star' : rating >= 2.5 ? "fa fa-star-half-alt" : 'fa fa-star-o'}></i>
             </span>
             <span>
-            <i className={rating >= 4 ? 'fa fa-star' : rating >= 3.5 ? "fa fa-star-half" : 'fa fa-star-o'}></i>
+            <i className={rating >= 4 ? 'fa fa-star' : rating >= 3.5 ? "fa fa-star-half-alt" : 'fa fa-star-o'}></i>
             </span>
             <span>
-            <i className={rating >= 5 ? 'fa fa-star' : rating >= 4.5 ? "fa fa-star-half" : 'fa fa-star-o'}></i>
+            <i className={rating >= 5 ? 'fa fa-star' : rating >= 4.5 ? "fa fa-star-half-alt" : 'fa fa-star-o'}></i>
             </span>
             <span>{numReviews +' reviews'}</span>
         </div >
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,44 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import Product from '../components/Product'
+import React, { useEffect, useReducer } from 'react';
+import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
+import MessageBox   from '../components/MessageBox'
+import axios from 'axios';
+import logger from 'use-reducer-logger';
 
-export default function HomeScreen() {
-    const [products, setProducts] = useState([]) //define el hook
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state;
+
+    }
+};
+
+function HomeScreen() {
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: '',
+    });
     useEffect(() => {
         const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' })
             try {
-                setLoading(true)
-                const { data } = await axios.get('/api/products');
-                setLoading(false)
-                setProducts(data);
+                // const { data } = await axios.get('/api/products');
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result });
             } catch (err) {
-                setError(err.message);
-                setLoading(false)
+                dispatch({ type: 'FETCH_FAIL', payload: err.message });
             }
         };
         fetchData();
     }, [])
     return (
         <div>
+
+            <div className="row center">
             {loading ? (
                 <LoadingBox></LoadingBox>
             ) :
                 error ? (
                     <MessageBox variant='danger'>{error}</MessageBox>
                 ) : (
-                    <div className="row center">
-                        {
-                            products.map(product => (
+                            products.data.map(product => (
                                 <Product key={product._id} product={product}></Product>
-                            ))
-                        }
+                            )))
+                }
 
-                    </div>
-                )}
+            </div>
+
         </div>
     )
 }
+export default HomeScreen
